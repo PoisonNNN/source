61bfc162ae7f6a2298e4d76e9398ce217ed338de
Scarecrow41 <83387458+Scarecrow41@users.noreply.github.com>
2022-04-09 15:15:07 +0800

    add post

---
+++ source/_posts/2022-04-09-CQ2008 & ZJ2009踩坑祭.cpp
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "「Solution」2022-04-09-CQ2008 & ZJ2009踩坑祭"
+date: 2022-04-09
+tags: [题解, 总结]
+image: http://gastonsanchez.com/images/blog/mathjax_logo.png
+
+---
+
+属于是把能踩的坑都跳了一遍。。。
+
+<!-- more -->
